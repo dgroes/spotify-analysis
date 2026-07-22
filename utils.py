f4194910b37d2a21e21e4fb634c96dd930374652
Diego Pasten <diegopasten78@gmail.com>
2026-07-21 23:16:34 -0400

    feat: :wrench: Se añaden más configuraciones de colores

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 RESET = '\033[0m'
+BOLD = "\x1b[1;39;49m"
 GREEN_B = "\x1b[1;38;5;77;49m"
 GREEN_I = "\x1b[3;38;5;77;49m"
 YELLOW_I = "\x1b[3;38;5;11;49m"
 YELLOW_B = "\x1b[1;38;5;11;49m"
+YELLOW_BI = "\x1b[1;3;38;5;11;49m"
 PINK = "\x1b[0;38;5;219;49m"
+PINK_B = "\x1b[1;38;5;219;49m"
 CYAN = "\x1b[0;38;5;80;49m"
